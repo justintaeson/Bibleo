5c6765c99e3fdc8ac98add2c9a9b1a79b42edde9
justintaeson <justintaeson@gmail.com>
2022-03-08 19:40:59 +0000

    Ensured that when a user clicks that save button, the verse is populate into the journal entries page. #3

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $newButton.addEventListener('click', generatePage);
 
 var $homePage = document.querySelector('#home-page');
 var $generatePage = document.querySelector('#generate-page');
+var $journalPage = document.querySelector('#journal-page');
 
 function generatePage(event) {
   $homePage.className = 'hidden';
@@ -22,7 +23,7 @@ function getVerse() {
     var $h3 = document.createElement('h3');
     $h3.id = 'verse-title';
     var $p = document.createElement('p');
-    $p.className = 'verse';
+    $p.id = 'verse';
 
     var $ul = document.querySelector('ul');
     $ul.appendChild($li);
@@ -81,15 +82,23 @@ function viewSwapper() {
   if (data.view === 'home-page') {
     $generatePage.className = 'hidden';
     $searchPage.className = 'hidden';
+    $journalPage.className = 'hidden';
     $homePage.className = 'container';
   } else if (data.view === 'generate-page') {
     $homePage.className = 'hidden';
     $searchPage.className = 'hidden';
+    $journalPage.className = 'hidden';
     $generatePage.className = 'container flex-wrap align-content-center';
   } else if (data.view === 'search-page') {
     $homePage.className = 'hidden';
     $generatePage.className = 'hidden';
+    $journalPage.className = 'hidden';
     $searchPage.className = 'container flex-wrap align-content-center';
+  } else if (data.view === 'journal-page') {
+    $homePage.className = 'hidden';
+    $generatePage.className = 'hidden';
+    $searchPage.className = 'hidden';
+    $journalPage.className = 'container flex-wrap';
   }
 }
 
@@ -116,7 +125,7 @@ function searchVerse(event) {
     var $h3 = document.createElement('h3');
     $h3.id = 'verse-title';
     var $p = document.createElement('p');
-    $p.className = 'verse';
+    $p.id = 'verse';
 
     var $ul = document.querySelector('ul');
     $ul.appendChild($li);
@@ -135,3 +144,36 @@ $continueReading.addEventListener('click', function (event) {
   var $verseTitle = document.getElementById('verse-title').textContent;
   $continueReading.setAttribute('href', 'https://www.biblegateway.com/passage/?search=' + $verseTitle + '&version=WEB');
 });
+
+// clicking save and creating the DOM tree for that verse
+var $saveButton = document.querySelector('#save-button');
+$saveButton.addEventListener('click', saveVerse);
+
+function saveVerse(event) {
+  var $ul = document.querySelector('#saved-entries');
+
+  var $li = document.createElement('li');
+  var $verseDiv = document.createElement('div');
+  var $verseP = document.createElement('p');
+  var $entryDiv = document.createElement('div');
+  var $entryP = document.createElement('p');
+
+  $ul.appendChild($li);
+  $li.appendChild($verseDiv);
+  $li.appendChild($entryDiv);
+  $verseDiv.appendChild($verseP);
+  $entryDiv.appendChild($entryP);
+
+  $li.className = 'desktop-display-flex';
+  $verseDiv.className = 'column-half';
+  $verseP.className = 'saved-verse-box';
+  $entryDiv.className = 'column-half';
+  $entryP.className = 'padding-around;';
+
+  var $verse = document.getElementById('verse').textContent;
+  var $verseTitle = document.getElementById('verse-title').textContent;
+  $verseP.textContent = $verse + ' - ' + $verseTitle;
+
+  data.view = 'journal-page';
+  viewSwapper();
+}
